37ef6642d54c8857958f6fad6be59704faaa8e65
Konstantinos Marinopoulos <km@itrust.digital>
2023-03-16 22:41:39 +0200

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Konstantinos Marinopoulos •',
@@ -11,8 +12,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <>
     <html lang="en">
+      
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-225RPY1284"/>
+      <Script id='google-analytics' strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-225RPY1284', {
+            page_path: window.location.pathname,
+          });`,
+        }}/>
+
       <body className="bg-dark-grey mt-[45px] mb-0 m-[25px] sm:m-[45px]">{children}</body>
     </html>
+    </>
+    
   )
 }
